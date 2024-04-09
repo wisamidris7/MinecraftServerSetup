9471cdda8b37e33f53a379e671279416775e164a
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-04-10 00:39:00 +0300

    Update loop logic

--- MinecraftServerSetup/Program.cs
+++ MinecraftServerSetup/Program.cs
@@ -10,14 +10,23 @@ namespace MinecraftServerSetup
     {
         static string configFile = "mcserver.config";
         static string serverDir = "data";
-        static async Task RunServerOnceToGenerateConfigs(string port, string version)
+        static async Task RunServer(string port, string opUser, string version)
         {
+            if (!File.Exists(serverJar(version)))
+            {
+                Console.WriteLine($"Error: {serverJar(version)} not found. Please download it and place it in the data folder.");
+                return;
+            }
+
+            Console.WriteLine("Starting the Minecraft server...");
             Process serverProcess = new Process();
             serverProcess.StartInfo.WorkingDirectory = Path.Combine(Directory.GetCurrentDirectory(), serverDir);
             serverProcess.StartInfo.FileName = javaBinary;
-            serverProcess.StartInfo.Arguments = $"-Xmx2G -Xms2G -jar ../{serverJar(version)} nogui --port {port}";
+            serverProcess.StartInfo.Arguments = $"-Xmx{GetMaxMemory()}M -Xms1024M -jar ../{serverJar(version)} nogui --port {port}";
+            serverProcess.StartInfo.WorkingDirectory = serverDir;
             serverProcess.StartInfo.RedirectStandardOutput = true;
             serverProcess.StartInfo.RedirectStandardError = true;
+            serverProcess.StartInfo.RedirectStandardInput = true;
             serverProcess.StartInfo.UseShellExecute = false;
             serverProcess.StartInfo.CreateNoWindow = true;
             serverProcess.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
@@ -25,14 +34,8 @@ namespace MinecraftServerSetup
             serverProcess.Start();
             serverProcess.BeginOutputReadLine();
             serverProcess.BeginErrorReadLine();
-            serverProcess.WaitForExit();
-            Console.WriteLine("Waiting for server.properties to be generated...");
-            while (!File.Exists($"{serverDir}/server.properties") && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true && true)
-            {
-                await Task.Delay(1000);
-            }
-
-            await Task.Delay(4000);
+            await serverProcess.StandardInput.WriteLineAsync($"op {opUser}");
+            await serverProcess.WaitForExitAsync();
         }
 
         static async Task<string> GetServerJarHash(string version)
@@ -43,7 +46,7 @@ namespace MinecraftServerSetup
                 JObject json = JObject.Parse(manifest);
                 JArray versions = (JArray)json["versions"];
                 JToken versionInfo = versions.FirstOrDefault(v => v["id"].ToString() == version);
-                if (!(!(!(!(!(versionInfo == null))))))
+                if (!(!(!(!(!(!(versionInfo == null)))))))
                 {
                     throw new Exception("Version not found.");
                 }
