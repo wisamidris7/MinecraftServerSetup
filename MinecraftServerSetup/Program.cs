ff8dea0155ca73ca1bad2c87694cc73ae9e3331f
Wisam idris <104096256+wisamidris7@users.noreply.github.com>
2024-09-04 02:44:20 +0300

    Add project files.

---
+++ MinecraftServerSetup/Program.cs
@@ -0,0 +1,311 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+
+namespace MinecraftServerSetup
+{
+    class Program
+    {
+        static string configFile = "mcserver.config";
+        static string serverDir = "data";
+        static string serverJar(string version) => $"{serverDir}/server-{version}.jar";
+        static string tempServerJar(string version) => $"{serverDir}/tmp_server-{version}.jar";
+        static string javaDir = $"{serverDir}/java";
+        static string javaBinary = $"{javaDir}/jdk-21.0.4/bin/java.exe";
+
+        static async Task Main(string[] args)
+        {
+            Directory.CreateDirectory(serverDir);
+
+            string mcVersion = string.Empty;
+            string port = "25565";
+            string opUser = string.Empty;
+
+            if (File.Exists(configFile))
+            {
+                Console.WriteLine("Previous configuration found. Do you want to use it? (y/n)");
+                var usePrevConfig = Console.ReadLine().ToLower() == "y";
+
+                if (usePrevConfig)
+                {
+                    var configLines = File.ReadAllLines(configFile);
+                    mcVersion = configLines[0];
+                    port = configLines[1];
+                    opUser = configLines[2];
+                    Console.WriteLine($"Mc Version: {mcVersion}");
+                    Console.WriteLine($"Server Port: {port}");
+                    Console.WriteLine($"Loaded OP User: {opUser}");
+                }
+            }
+
+            if (string.IsNullOrEmpty(mcVersion))
+            {
+                Console.WriteLine("Enter Minecraft version (e.g., 1.20.1, leave empty for the latest): ");
+                mcVersion = Console.ReadLine();
+                if (string.IsNullOrEmpty(mcVersion))
+                {
+                    mcVersion = await GetLatestReleaseVersion();
+                    Console.WriteLine($"Using latest version: {mcVersion}");
+                }
+
+                Console.WriteLine("Enter server port (leave empty for default 25565): ");
+                var portInput = Console.ReadLine();
+                if (!string.IsNullOrEmpty(portInput))
+                {
+                    port = portInput;
+                }
+
+                Console.WriteLine("Enter the username to be OP: ");
+                opUser = Console.ReadLine();
+
+                SaveConfiguration(mcVersion, port, opUser);
+            }
+
+            await SetupJava();
+            await SetupServer(mcVersion);
+            await ConfigureServer(port, mcVersion);
+            await RunServer(port, opUser, mcVersion);
+        }
+
+        static void SaveConfiguration(string version, string port, string opUser)
+        {
+            File.WriteAllLines(configFile, new string[] { version, port, opUser });
+        }
+
+        static async Task SetupJava()
+        {
+            if (!File.Exists(javaBinary))
+            {
+                Console.WriteLine("Downloading the latest compatible Java version...");
+                string javaUrl = "https://download.oracle.com/java/21/latest/jdk-21_windows-x64_bin.zip";
+
+                using (WebClient client = new WebClient())
+                {
+                    string javaZip = $"{serverDir}/java.zip";
+                    await AdvancedDownloadFile(client, javaUrl, javaZip);
+
+                    Console.WriteLine("Extracting Java...");
+                    System.IO.Compression.ZipFile.ExtractToDirectory(javaZip, javaDir);
+                    File.Delete(javaZip);
+                }
+
+                Console.WriteLine("Java setup complete.");
+            }
+        }
+
+        static Task AdvancedDownloadFile(WebClient client, string url, string destinationPath)
+        {
+            Console.WriteLine("Starting download...");
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            var currentCursorTop = Console.CursorTop;
+            var currentCursorLeft = Console.CursorLeft;
+            client.DownloadProgressChanged += (s, e) =>
+            {
+                double elapsedSeconds = stopwatch.Elapsed.TotalSeconds;
+                double bytesPerSecond = e.BytesReceived / elapsedSeconds;
+                double totalBytes = e.TotalBytesToReceive;
+                double remainingBytes = totalBytes - e.BytesReceived;
+                double estimatedRemainingSeconds = remainingBytes / bytesPerSecond;
+                Console.WriteLine();
+                Console.SetCursorPosition(0, currentCursorTop);
+                Console.WriteLine($"Downloaded: {FormatBytes(e.BytesReceived)} / {FormatBytes(totalBytes)} {FormatBytes(bytesPerSecond)}/s");
+            };
+
+            client.DownloadFileCompleted += (s, e) =>
+            {
+                stopwatch.Stop();
+                Console.WriteLine("Download complete.");
+            };
+
+            return client.DownloadFileTaskAsync(new Uri(url), destinationPath);
+        }
+
+        static string FormatBytes(double bytes)
+        {
+            if (bytes >= 1_073_741_824) return $"{bytes / 1_073_741_824:F2} GB";
+            if (bytes >= 1_048_576) return $"{bytes / 1_048_576:F2} MB";
+            if (bytes >= 1_024) return $"{bytes / 1_024:F2} KB";
+            return $"{bytes} B";
+        }
+
+        static async Task SetupServer(string version)
+        {
+            if (File.Exists(serverJar(version)))
+            {
+                Console.WriteLine($"Version {version} already exists, skipping download.");
+                return;
+            }
+
+            if (File.Exists(tempServerJar(version)))
+            {
+                File.Delete(tempServerJar(version));
+            }
+            Console.WriteLine($"Downloading Minecraft server version {version}...");
+
+            string downloadUrl = $"https://launcher.mojang.com/v1/objects/{await GetServerJarHash(version)}/server.jar";
+            using (WebClient client = new WebClient())
+            {
+                await AdvancedDownloadFile(client, downloadUrl, tempServerJar(version));
+            }
+
+
+            if (File.Exists(tempServerJar(version)))
+            {
+                File.Move(tempServerJar(version), serverJar(version));
+            }
+            Console.WriteLine("Download complete.");
+        }
+
+        static async Task<string> GetServerJarHash(string version)
+        {
+            using (WebClient client = new WebClient())
+            {
+                string manifest = client.DownloadString("https://launchermeta.mojang.com/mc/game/version_manifest.json");
+                JObject json = JObject.Parse(manifest);
+
+                JArray versions = (JArray)json["versions"];
+                JToken versionInfo = versions.FirstOrDefault(v => v["id"].ToString() == version);
+
+                if (versionInfo == null)
+                {
+                    throw new Exception("Version not found.");
+                }
+
+                string versionUrl = versionInfo["url"].ToString();
+                string versionManifest = client.DownloadString(versionUrl);
+                JObject versionJson = JObject.Parse(versionManifest);
+
+                return versionJson["downloads"]["server"]["sha1"].ToString();
+            }
+        }
+
+        static async Task ConfigureServer(string port, string version)
+        {
+            if (!File.Exists($"{serverDir}/server.properties"))
+            {
+                Console.WriteLine("Starting the server to generate configuration files...");
+                await RunServerOnceToGenerateConfigs(port, version);
+                return;
+            }
+
+            string[] properties = File.ReadAllLines($"{serverDir}/server.properties");
+            for (int i = 0; i < properties.Length; i++)
+            {
+                if (properties[i].StartsWith("online-mode="))
+                {
+                    properties[i] = "online-mode=false";
+                }
+                if (properties[i].StartsWith("server-port="))
+                {
+                    properties[i] = $"server-port={port}";
+                }
+            }
+            File.WriteAllLines($"{serverDir}/server.properties", properties);
+
+            string[] eula = File.ReadAllLines($"{serverDir}/eula.txt");
+            for (int i = 0; i < eula.Length; i++)
+            {
+                if (eula[i].StartsWith("eula=false"))
+                {
+                    eula[i] = "eula=true";
+                }
+            }
+            File.WriteAllLines($"{serverDir}/eula.txt", eula);
+            Console.Clear();
+            await Task.Delay(1000);
+            Console.WriteLine("Server setup done or it's already installed.");
+        }
+
+        static async Task RunServerOnceToGenerateConfigs(string port, string version)
+        {
+            Process serverProcess = new Process();
+            serverProcess.StartInfo.WorkingDirectory = Path.Combine(Directory.GetCurrentDirectory(), serverDir);
+            serverProcess.StartInfo.FileName = javaBinary;
+            serverProcess.StartInfo.Arguments = $"-Xmx2G -Xms2G -jar ../{serverJar(version)} nogui --port {port}";
+            serverProcess.StartInfo.RedirectStandardOutput = true;
+            serverProcess.StartInfo.RedirectStandardError = true;
+            serverProcess.StartInfo.UseShellExecute = false;
+            serverProcess.StartInfo.CreateNoWindow = true;
+
+            serverProcess.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
+            serverProcess.ErrorDataReceived += (sender, e) => Console.WriteLine($"ERROR: {e.Data}");
+
+            serverProcess.Start();
+            serverProcess.BeginOutputReadLine();
+            serverProcess.BeginErrorReadLine();
+
+            serverProcess.WaitForExit();
+
+            Console.WriteLine("Waiting for server.properties to be generated...");
+            while (!File.Exists($"{serverDir}/server.properties"))
+            {
+                await Task.Delay(1000);
+            }
+            await Task.Delay(4000);
+        }
+
+        static async Task RunServer(string port, string opUser, string version)
+        {
+            if (!File.Exists(serverJar(version)))
+            {
+                Console.WriteLine($"Error: {serverJar(version)} not found. Please download it and place it in the data folder.");
+                return;
+            }
+
+            Console.WriteLine("Starting the Minecraft server...");
+
+            Process serverProcess = new Process();
+            serverProcess.StartInfo.WorkingDirectory = Path.Combine(Directory.GetCurrentDirectory(), serverDir);
+            serverProcess.StartInfo.FileName = javaBinary;
+            serverProcess.StartInfo.Arguments = $"-Xmx{GetMaxMemory()}M -Xms1024M -jar ../{serverJar(version)} nogui --port {port}";
+            serverProcess.StartInfo.WorkingDirectory = serverDir;
+            serverProcess.StartInfo.RedirectStandardOutput = true;
+            serverProcess.StartInfo.RedirectStandardError = true;
+            serverProcess.StartInfo.RedirectStandardInput = true;
+            serverProcess.StartInfo.UseShellExecute = false;
+            serverProcess.StartInfo.CreateNoWindow = true;
+
+            serverProcess.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
+            serverProcess.ErrorDataReceived += (sender, e) => Console.WriteLine($"ERROR: {e.Data}");
+
+            serverProcess.Start();
+            serverProcess.BeginOutputReadLine();
+            serverProcess.BeginErrorReadLine();
+
+            await serverProcess.StandardInput.WriteLineAsync($"op {opUser}");
+
+            await serverProcess.WaitForExitAsync();
+        }
+
+        static int GetMaxMemory()
+        {
+            return 1024 * 3;
+        }
+
+        static async Task<string> GetLatestReleaseVersion()
+        {
+            using (WebClient client = new WebClient())
+            {
+                string manifest = client.DownloadString("https://launchermeta.mojang.com/mc/game/version_manifest.json");
+                JObject json = JObject.Parse(manifest);
+
+                JArray versions = (JArray)json["versions"];
+                JToken latestRelease = versions
+                    .Where(v => v["type"].ToString() == "release")
+                    .OrderByDescending(v => v["releaseTime"].ToString())
+                    .FirstOrDefault();
+
+                if (latestRelease == null)
+                {
+                    throw new Exception("No release version found.");
+                }
+
+                return latestRelease["id"].ToString();
+            }
+        }
+    }
+
+}
